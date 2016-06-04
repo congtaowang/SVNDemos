155532a632ecfed08f1c53e1691d14abec194de5
congtaowang <congtaowang225@gmail.com>
2016-06-04 10:28:42 +0800

    update to v1.0.1

--- app/src/main/java/ct/svndemo/MainActivity.java
+++ app/src/main/java/ct/svndemo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Update to V1.0.1");
+        System.out.println("Update to V1.0.2");
     }
 }
